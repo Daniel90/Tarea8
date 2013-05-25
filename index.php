2d492f136c9ac7d9e732bdbb275bf5bd2833f783
Daniel Abrilot <daniel.abrilot@gmail.com>
2013-05-24 17:58:48 -0700

    first

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    </head>
+    <body>
+        <form action = "datos.php" method = "post" name = "formulario">
+            <label>Rut</label>&nbsp&nbsp&nbsp<input type = "text" name = "rut"></br></br>
+            <label>password</label>&nbsp&nbsp&nbsp<input type = "password" name = "pw"></br></br>
+            <input type = "submit" value = "Aceptar">
+        </form>
+    </body>
+</html>
+
